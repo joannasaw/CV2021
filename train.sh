7504b695bab5d54101c57f42628a0ff3c5967b85
ianlimle <33076925+ianlimle@users.noreply.github.com>
2021-05-02 04:13:17 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd CODE
-python pipeline_test.py --model vgg16_lstm
-python pipeline_test.py --model vgg16_fpm_lstm
-python pipeline_test.py --model vgg16_lstm_attention
-python pipeline_test.py --model vgg16_fpm_lstm_attention
-python pipeline_test.py --model vgg16_fpm_blstm_attention
+python pipeline.py --model vgg16_lstm
+python pipeline.py --model vgg16_fpm_lstm
+python pipeline.py --model vgg16_lstm_attention
+python pipeline.py --model vgg16_fpm_lstm_attention
+python pipeline.py --model vgg16_fpm_blstm_attention
